181f3bce56641190070d6a0be226e2120437fcf8
necojarac <trustno_one@mqb.biglobe.ne.jp>
2021-08-31 20:59:04 +0900

    mapを使ったトリボナッチ数計算の実装

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 'use strict';
+const length = process.argv[2] || 0;
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
+memo.set(2, 1);
 function trib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
+    const value = memo.get(n - 1) + memo.get(n - 2) + memo.get(n - 3);
     memo.set(n, value);
     return value;
 }
-const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+    console.log(`${i}番目のトリボナッチ数は${trib(i)}`);
 }
